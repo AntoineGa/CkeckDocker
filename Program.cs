4898878ca4d2da46bbe5428a8f0738447eeb9841
Antoine <drin666@hotmail.com>
2017-11-01 19:29:44 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Docker.DotNet;
+
+namespace CkeckDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DockerClient client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine"))
+                    .CreateClient();
+            
+        }
+    }
+}
